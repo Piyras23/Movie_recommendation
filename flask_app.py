60809f503e1a545d1bfa721514b8266b318d0dbc
Piyush Rastogi <piyushrastogi311@gmail.com>
2023-11-12 16:50:29 -0800

    Flask_v2

--- flask_app.py
+++ flask_app.py
@@ -3,27 +3,22 @@ import pandas as pd
 from surprise import Dataset, Reader, KNNBasic
 from surprise.model_selection import train_test_split
 
-
 app = Flask(__name__)
 
-# Load data only rating and movies 
+# Load data and train the model
 df_rating = pd.read_csv('ratings.csv')
 df_movies = pd.read_csv('movies.csv')
 
-# Cross-validation: 80-20 split
 reader = Reader(rating_scale=(0.5, 5))
 data = Dataset.load_from_df(df_rating[['userId', 'movieId', 'rating']], reader)
-trainset, testset = train_test_split(data, test_size=0.2)
+trainset, _ = train_test_split(data, test_size=0.2)
 
 # Train the model using KNNBasic with cosine similarity
 sim_options = {'name': 'cosine', 'user_based': True}
 model = KNNBasic(sim_options=sim_options)
 model.fit(trainset)
 
-@app.route('/user_recommendations', methods=['GET'])
-def recommend_movies():
-    user_id = int(request.args.get('user_id', default=1))
-
+def get_user_recommendations(user_id, N=5):
     # Unrated movies for the user
     user_movies = df_rating[df_rating['userId'] == user_id]['movieId'].unique()
     user_unrated_movies = df_rating[~df_rating['movieId'].isin(user_movies)]['movieId'].unique()
@@ -38,13 +33,22 @@ def recommend_movies():
     # Merging with movie titles
     user_recommendations = pd.merge(user_predictions_df, df_movies[['movieId', 'title']], on='movieId')
 
-    # Get top 5 recommendations
-    user_top_n = user_recommendations.sort_values(by='estimated_rating', ascending=False).head(5)
+    # Get top N recommendations
+    user_top_n = user_recommendations.sort_values(by='estimated_rating', ascending=False).head(N)
 
-    # Convert the recommendations to JSON
-    recommendations_json = user_top_n[['title', 'estimated_rating']].to_dict(orient='records')
+    return user_top_n[['title', 'estimated_rating']]
 
-    return jsonify(recommendations_json)
+@app.route('/user_recommendations', methods=['GET'])
+def recommend_movies():
+    # Extract user_id from the request parameters
+    user_id = int(request.args.get('user_id'))
+
+    # Get recommendations for the specified user_id
+    recommendations = get_user_recommendations(user_id)
+
+    # Convert recommendations to JSON and return
+    return jsonify(recommendations.to_dict(orient='records'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
