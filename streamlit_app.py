a2f1dd9e99b4f75fcc1dbf44c0016b448ae1ce9c
Piyush Rastogi <piyushrastogi311@gmail.com>
2023-11-13 05:41:18 -0800

    streamlit_module_v8

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,21 @@
 import streamlit as st
 import requests
 
-# Function to get movie recommendations from Flask API
+# Moovie recommendations from Flask API
 def get_movie_recommendations(user_id):
-    url = f"http://172.17.0.2:5001/user_recommendations?user_id={user_id}"
+    url = "http://flask_container_name:5001/user_recommendations?user_id={user_id}"
+
     response = requests.get(url)
     data = response.json()
     return data
 
-# Streamlit app code
+# Processing 
 st.title("Movie Recommendation App")
 
 # User input for user ID
 user_id = st.number_input("Enter User ID:", min_value=1, max_value=100, value=1, step=1)
 
-# Get movie recommendations
+# Retreive movie recommendations
 recommendations = get_movie_recommendations(user_id)
 
 # Display recommendations and influencing factors
